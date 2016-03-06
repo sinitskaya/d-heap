bd04e29f32228d2b88cc2eda6faa463d51cf8403
sinitskaya <o.sinitskayaf@yandex.ru>
2016-03-06 23:45:32 +0300

    методы

--- include/d-heap.h
+++ include/d-heap.h
@@ -1,3 +1,32 @@
 #pragma once
 #include <iostream>
 using namespace std;
+typedef int VT;
+class D_heap 
+{
+private:
+	int size;                  //число элементов в куче
+	int d;                     //число потомков
+	int Islist(int i);         //проверка эл-та на лист
+public:   	
+	VT *keys;        
+	D_heap(int s, int d);
+	D_heap(const D_heap & heap);
+	~D_heap();
+
+	int GetSize();
+	void Swap(int i, int j);            //транспонирование
+	void siftUp(int i);                 //всплытие
+	void siftDown(int i);               //погружение
+	void DeleteMinKey();                //удаление с мин ключом
+	void DeleteIndex(int i);            //удаление с заданным индексом
+	void Heapify();                     //окучивание
+	int MinChild(int i);                 //индекс минимального потомка
+	void InsertKey(VT key);             //вставка нового ключа
+	int Hight();                        //кол-во слоев(высота)
+	void Print();
+
+	int operator ==(const D_heap &heap)const;
+	int operator !=(const D_heap &heap)const;
+	D_heap& operator =(const D_heap &heap);
+};
