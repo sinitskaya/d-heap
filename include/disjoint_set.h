6bbd17459b8a5d3bdd68daa551d47086a8e38757
sinitskaya <o.sinitskayaf@yandex.ru>
2016-03-19 20:02:14 +0300

    разделенные множества

---
+++ include/disjoint_set.h
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+class Disjoint_set
+{
+private:
+	int *h;            //ранг-высота поддерева, корнем которого является узел x
+	int *p;            //родитель для узла x
+	int maxsize;       //макс кол элементов в множестве(мощность)
+public:
+	Disjoint_set();
+	Disjoint_set(int s);
+	~Disjoint_set();
+
+	void CreateSingleton(const int x);  //создание синглтона
+	void Union(int x, int y);           //обьединение множеств
+	int FindSubset(int x);              //имя множества, которому принадлежит x
+	void Errors(int x);
+	int GetMaxsize();
+	void Print();
+};
